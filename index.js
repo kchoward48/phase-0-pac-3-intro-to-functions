5f1885a91258d349db3caf11fb9c880d7e449ffd
Kevin Howard <kchoward48@gmail.com>
2021-06-25 16:37:09 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,72 @@
 // Follow along with the examples here
+
+function sayHello() {
+    console.log('Hello!');
+  }
+
+sayHello();
+
+function sayHelloToIsabel() {
+    console.log("Hello, Isabel!");
+}
+
+sayHelloToIsabel();
+
+function sayHelloToSofia() {
+    console.log("Hello, Sofia!");
+}
+
+sayHelloToSofia();
+
+function sayHelloToBrendan() {
+    console.log("Hello, Brendan!");
+}
+
+sayHelloToBrendan();
+
+function doSomething(thing) {
+    return 0;
+    console.log(thing);
+}
+
+console.log(doSomething(['Roads to Judah','Sunbather',
+    'New Bermuda', 'Ordinary Corrupt Human Love']));
+
+function sayHelloTo(firstName) {
+    console.log(`Hello, ${firstName}!`);
+}
+
+sayHelloTo('Isabel');
+sayHelloTo("Jane");
+sayHelloTo("R2-D2");
+sayHelloTo(1);
+
+// console.log(firstName);
+
+function say(greeting, firstName) {
+    return `${greeting}, ${firstName}!`;
+}
+
+say("Goodbye", "Julio");
+
+say("Julio", "hello");
+
+console.log(say("Hello", "Sofia"))
+
+function add(x, y) {
+    return x + y;
+}
+
+console.log(add(1, 2));
+console.log(add(80, 9000));
+
+function toCelsius(fahrenheit) {
+    console.log(fahrenheit);
+    return (5/9) * (fahrenheit - 32);
+}
+
+console.log(toCelsius(getRandomInt(500)));
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
